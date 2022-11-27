3981d583b6e02c5036ebe295de946df20d847eb4
devlitus <developercarles@gmail.com>
2022-11-27 09:44:27 +0100

    styles

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,6 +1,6 @@
 import './Header.css'
 export const Header = ({ children, className }) => {
-  const style = className ? `header ${className}` : 'header'
+  const style = className === undefined ? 'header' : className
   return (
     <header className={style}>{ children }</header>
   )
