f7ec8330e38c629c996d81233ed070ed06b6e05b
devlitus <developercarles@gmail.com>
2022-12-06 08:37:15 +0100

    remove files

--- src/Header.jsx
+++
@@ -1,7 +0,0 @@
-import './Header.css'
-export const Header = ({ children, className }) => {
-  const style = className === undefined ? 'header' : className
-  return (
-    <header className={style}>{ children }</header>
-  )
-}
